2592a57c7905465e867d08a60660e0c4ae382b7f
Tolik <dothaiod@gmail.com>
2024-09-06 15:26:20 +0300

    progress

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    
+    int a, b;
+
+    std::cout << "¬ведите два числа: ";
+    std::cin >> a >> b;
+
+    int sum = a + b;
+
+    std::cout << "—умма: " << sum << std::endl;
+
+    return 0;
 }
